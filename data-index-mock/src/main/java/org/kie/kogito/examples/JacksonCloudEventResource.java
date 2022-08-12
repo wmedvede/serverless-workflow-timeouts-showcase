500b0f0c95fabffc650d6b6496f8defff753b255
Walter Medvedeo <wmedvedeo@gmail.com>
2022-08-12 15:42:52 +0200

    Updates 1

--- data-index-mock/src/main/java/org/kie/kogito/examples/JacksonCloudEventResource.java
+++ data-index-mock/src/main/java/org/kie/kogito/examples/JacksonCloudEventResource.java
@@ -15,21 +15,29 @@
  */
 package org.kie.kogito.examples;
 
-import java.time.ZonedDateTime;
-
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.cloudevents.CloudEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/")
-public class DataIndexMockResource {
+public class JacksonCloudEventResource {
 
-    @Path("/hello")
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from DataIndexMock: " + ZonedDateTime.now();
+    private static final Logger LOGGER = LoggerFactory.getLogger(JacksonCloudEventResource.class);
+
+    @Path("/cloud-event")
+    @POST
+    @Consumes("*/*")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response onCloudEvent(CloudEvent event) {
+        LOGGER.debug("Event received: {}", event);
+        return Response.accepted().build();
     }
 
 }
